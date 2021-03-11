1e2bbc589cb7589652be7086d441cd3822e882d2
Ramil Garipov <ramillion@ya.ru>
2021-03-11 11:10:40 +0300

    Create stage.js

---
+++ stage.js
@@ -0,0 +1,31 @@
+window.appConfig = {
+  clientId: "e658f6511ea84e158842b657cc02f609",
+  baseUrl: "fxgstage.com",
+  statusUrl: "https://status.drixx.com",
+  logo: {
+    darkThemeUrl:
+        "https://uploads-ssl.webflow.com/5f478771e161508c2a99cdd5/5fc530ab785eba6b4e47cd41_logo-drixx.svg",
+    lightThemeUrl:
+        "https://uploads-ssl.webflow.com/5f478771e161508c2a99cdd5/5f58d03beed8d3323efbcfb9_LOGO.svg",
+  },
+  termsUrl: "https://www.drixx.com/terms-conditions",
+  defaultSymbol: "BTC-PERP",
+  homepageUrl: "/",
+  defaultLang: "en-US",
+  defaultTheme: "dark",
+  translations: {
+    "en-US": {
+      // possible keys can be found here:
+      // https://bitbucket.org/nesthorng/tradingplatform/src/master/src/translations/en.generated.json
+      // https://bitbucket.org/nesthorng/tradingplatform/src/master/src/translations/en.json
+      "appMenu.perpetual": "Perpetual",
+      "appMenu.reference": "Reference",
+      "appMenu.yield": "Yield",
+      "pages.yield.banner.title": "With Drixx Yield your stablecoins are worth more",
+      "pages.yield.banner.subTitle": "Earn up to 18% APY on your crypto, daily",
+    },
+    "ru-RU": {
+      "appMenu.reference": "Справочник",
+    },
+  },
+};
